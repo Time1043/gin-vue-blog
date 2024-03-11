da09603329b9c8e3864b5def72da194952ad908b
time1043 <1665434994@qq.com>
2024-03-11 20:53:55 +0800

    封装响应

--- gvb-server/api/settings_api/settings_info.go
+++ gvb-server/api/settings_api/settings_info.go
@@ -1,7 +1,17 @@
 package settings_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time1043/gvb-server/models/res"
+)
 
 func (SettingApi) SettingsInfoView(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{"msg": "xxx"})
+	res.Ok(map[string]string{}, "xxx", ctx)
+	res.OkWithData(map[string]string{"id": "1043"}, ctx)
+	res.OkWithMessage("很成功", ctx)
+
+	res.Fail(map[string]string{}, "xxx", ctx)
+	res.FailWithMessage("很失败", ctx)
+	res.FailWithCode(res.SettingsError, ctx)
+	res.FailWithCode(2, ctx)
 }
--- gvb-server/main.go
+++ gvb-server/main.go
@@ -7,22 +7,10 @@ import (
 )
 
 func main() {
-	core.InitConf() // 读取配置文件
-	//fmt.Println(global.Config) // 验证成功
-
+	core.InitConf()                // 读取配置文件
 	global.Log = core.InitLogger() // 初始化日志
-	//global.Log.Warnln("ha ha ha ")
-	//global.Log.Error("ha ha ha ")
-	//global.Log.Info("ha ha ha ")
-	//logrus.Warnln("ha ha ha ")
-	//logrus.Error("ha ha ha ")
-	//logrus.Info("ha ha ha ")
-
-	global.DB = core.InitGorm() // 连接数据库
-	//println(global.DB)          // 验证过
-
-	// 路由配置
-	router := routers.InitRouter()
+	global.DB = core.InitGorm()    // 连接数据库
+	router := routers.InitRouter() // 路由配置
 	addr := global.Config.System.Addr()
 	global.Log.Infof("gvb-server 运行在： %s", addr)
 	router.Run(addr) // 验证成功

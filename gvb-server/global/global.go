40cb9de525a57c832aa19fe8dbf013eeabecca67
time1043 <1665434994@qq.com>
2024-03-11 16:37:19 +0800

    配置初始化：日志初始化、gorm初始化

--- gvb-server/global/global.go
+++ gvb-server/global/global.go
@@ -1,7 +1,13 @@
 package global
 
-import "time1043/gvb-server/config"
+import (
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+	"time1043/gvb-server/config"
+)
 
 var (
-	Config *config.Config // 指针 需要修改
+	Config *config.Config // yaml配置信息 (指针 需要修改)
+	Log    *logrus.Logger // 日志配置
+	DB     *gorm.DB       // gorm
 )
--- gvb-server/main.go
+++ gvb-server/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"github.com/sirupsen/logrus"
 	"time1043/gvb-server/core"
 	"time1043/gvb-server/global"
 )
 
 func main() {
 	core.InitConf() // 读取配置文件
-	fmt.Println(global.Config)
+	//fmt.Println(global.Config) // 验证成功
+
+	global.Log = core.InitLogger() // 初始化日志
+	global.Log.Warnln("ha ha ha ")
+	global.Log.Error("ha ha ha ")
+	global.Log.Info("ha ha ha ")
+	logrus.Warnln("ha ha ha ")
+	logrus.Error("ha ha ha ")
+	logrus.Info("ha ha ha ")
+
+	global.DB = core.InitGorm() // 连接数据库
+	//println(global.DB)          // 验证过
 }
